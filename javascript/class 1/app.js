a244a26c1458cbd28624371703519e1277c0e3f4
hasanCoder66 <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2026-02-23 09:54:43 +0500

    23 feb 2026

---
+++ javascript/class 1/app.js
@@ -0,0 +1,20 @@
+// document.writeln("shafa ullah")
+// console.log("Hello world")
+// document.writeln("hello")
+// document.writeln(5 * "abc")
+
+
+// document.writeln("<h1> hello <br> world </h1>")
+
+alert("hello \nworld")
+
+
+
+
+
+
+
+
+
+
+
---
+++ javascript/class 2/app.js
@@ -0,0 +1,29 @@
+// var user = "Saad"
+// var user = "Ali"
+// document.writeln(user)
+
+// var user = "talha"
+// var age = 13
+
+// document.writeln(
+//     "My name is " + user
+//     + "<br> My age is " + age
+// )
+
+// var kamran = "Ali"
+// var num1 = "10"
+// var num2 = kamran
+
+// document.writeln(num1 + num2)
+
+// document.writeln(4 - 4 + "5" * "2 * 2" + 10 * 2)
+
+// 40
+// 20 20
+// NAN
+// 52220
+
+document.writeln((2 * 4) * (4 + 2))
+
+
+
