f9e3f077c43799116dd3ea6bb2d0dace74fb096e
hasanCoder66 <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2026-02-25 09:58:07 +0500

    25 feb 2026

---
+++ javascript/class 3/app.js
@@ -0,0 +1,40 @@
+// post inc
+// pre inc
+// post dec
+// pre dec
+
+// var c = 5
+// var d = 2
+
+// var eq = d++ + ++d + d++ + c++ + d++ + ++c + c++ + ++c + d++ + c++
+
+// document.writeln(eq)
+
+// 44 hassan
+// 58
+// 59
+// 46
+// 48
+// 54
+// 30
+// 57
+// 73
+// 61
+// 59
+
+
+// var a = 0
+// var b = 2
+// var c = 4
+
+// var eq = b++ + ++c + a++ + ++a + ++a + a++ + b++ + b + c++ + c++ + ++a + ++c + b++ + a + b 
+          
+// document.writeln(eq)
+
+var a = 2
+var b = 3
+var c = 5
+
+var eq = a++ + c++ + ++b + b++ + c++ + a++ + ++a + b++ + ++c + c++ + b + c + a++ + a + c++ + a
+          
+document.writeln(eq)
