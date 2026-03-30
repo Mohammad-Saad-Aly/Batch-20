ad888c493cb35fd91a7e746e343d6f3f5c7a2177
hasanCoder66 <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2026-03-30 10:39:18 +0500

    300326

---
+++ javascript/class 6/app.js
@@ -0,0 +1,27 @@
+// collection of data 
+
+
+// methods of array
+
+// push daalna akhir se
+// pop  nikaalna akhir se 
+// shift nikaal shru se
+// unshift daalna shru se
+// splice kahin se bhi daalna or kahin se bhi nikaalna
+// slice copy karta hai or new array banata
+
+// names.push("anas", "hassan")
+// names.pop()
+// names.unshift("yaqoob")
+// names.shift()
+// names.shift()
+// names.shift()
+
+var names = ["saad", "ali", "fahad", "rehman", "bilal"]
+
+names.splice(1, 3, "khizer")
+
+console.log(names)
+
+
+
