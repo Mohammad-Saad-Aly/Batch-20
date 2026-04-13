cd00a8120e7cc76f080458e6dbd2bd7b3c8f66fa
hasanCoder66 <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2026-04-13 10:56:27 +0500

    13 04 2026

---
+++ javascript/class 10/app.js
@@ -0,0 +1,38 @@
+
+// var cars = ['mercedes', 'civic', 'toyota', 'cherry', 'jaeco']
+// var customerCar = prompt("bhai konsi gari chahye ?")
+// var match = false 
+
+// for (var i = 0; i < cars.length; i++) {
+//     if (customerCar == cars[i]) {
+//         match = true
+//         console.log(customerCar + " available")
+//         break
+//     }
+// }
+
+// if(match == false){
+//     console.log(customerCar + " not available")
+// }
+
+// break
+// continue
+
+
+// for(var i=1; i < 11; i++){
+//     if( i == 5 ){
+//         continue
+//     }
+//     document.write(i + "<br>")
+// }
+
+// var nums = [
+//     [0, 1, 2, 3],
+//     [1, 0, 1, 2],
+//     [2, 1, 0, 1]
+// ]
+
+// for (var i = 0; i < nums.length; i++){
+//     document.write(nums[i].join(" "), "<br>")
+// }
+
