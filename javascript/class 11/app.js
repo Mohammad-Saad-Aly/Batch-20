77f07e0334733530eeb39828553ed8b92c793d3c
hasanCoder66 <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2026-04-15 10:54:12 +0500

    15 04 2026

---
+++ javascript/class 11/app.js
@@ -0,0 +1,89 @@
+// var students = ["ahmed", "asad", "hadi", "hamdan"]
+// var teacher = ["Sir Saad", "Sir Faizan"]
+
+// for (var i = 0; i < students.length; i++) { 
+
+//     for (var j = 0; j < teacher.length; j++) {
+//         document.write(students[i] + " " + teacher[j] + "<br>")
+//     }
+
+//     document.write("<hr>")
+
+// }
+
+// ahmed sir saad
+// ahmed sir faizan
+
+// asad 
+
+
+
+// ahmed sir saad
+// ahmed sir faizan
+
+// asad sir saad
+// asad sir faizan
+
+// .....
+
+
+// for (var i = 0; i < students.length; i++) {
+//     document.write(students[i] + " " + teacher[i] + "<br>")
+// }
+
+
+
+
+// for (var i = 1; i <= 100; i = i + 10) {
+//     for(var j = i; j < i + 10; j++){
+//         document.write(j + " ")
+//     }
+//     document.write("<br>")
+// }
+
+
+// 1 2 3 4 5 6 7 8 9 10
+// 11 12 13 14 15 16 17 18 19 20
+
+// 91 92 93 94 95 96 97 98 99 100
+
+// 1 
+// 1 2 
+// 1 2 3 
+// 1 2 3 4
+// 1 2 3 4 5
+
+
+for (var i = 1; i <= 5; i++) {
+    for (var j = 1; j <= i; j++) {
+        document.write("*" + " ")
+    }
+    document.write("<br>")
+}
+
+// 5
+// 5 4
+// 5 4 3
+// 5 4 3 2
+// 5 4 3 2 1
+
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+
+
+
+
+
+
+
+
