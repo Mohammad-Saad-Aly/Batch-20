b37302dd92c0ac2c25e278675cb5f5c8636a8cd1
hasanCoder66 <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2026-04-17 10:54:53 +0500

    17 04 2026

---
+++ javascript/class 12/app.js
@@ -0,0 +1,32 @@
+// var user = prompt("Enter any word")
+// var ubaid = user[0].toUpperCase()
+// var saqib = user.slice(1).toLowerCase()
+// var concat = ubaid + saqib
+// console.log(concat)
+
+// var pyaareLarky = ["Kamran", "Shafaullah", "Topi", "Hassan", "Haseeb"]
+// var customer = prompt("konsa chahye larka ?")
+// var match = false
+
+// var firstAplha = customer.slice(0,1).toUpperCase()
+// var remainingAplha = customer.slice(1).toLowerCase()
+// var merge = firstAplha + remainingAplha 
+
+
+// for (var i = 0; i < pyaareLarky.length; i++) {
+//     if (merge == pyaareLarky[i]) {
+//         match = true
+//         console.log(merge + " larka available")
+//     }
+// }
+
+// if(match == false) {
+//     console.log(merge + " larka not available")
+// }
+
+
+var fruits = ["apple", "banana", "cherry", "banana", "mango", "orange"]
+
+console.log(fruits.lastIndexOf("banana", 2))
+
+
