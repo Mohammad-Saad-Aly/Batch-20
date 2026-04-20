c3d7ba402dfabe3e11be5bc124b3590034a18bad
hasanCoder66 <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2026-04-20 10:46:07 +0500

    20 4 2026

---
+++ javascript/class 13/app.js
@@ -0,0 +1,32 @@
+// var str = "hello war hello war"
+// console.log(str.replaceAll('world', "pakistan"))
+
+
+// var num = 74.9
+// console.log(Math.floor(num))
+
+
+// var num = (Math.random() * 10).toFixed(0)
+// console.log(num)
+
+
+// var heads = prompt("Enter player 1 name")
+// var tails = prompt("Enter player 2 name")
+
+// var toss = Math.floor(Math.random() * 2)
+
+// if (toss == 0) {
+//     console.log(heads + " heads win the toss")
+// }
+// else {
+//     console.log(tails + " tails win the toss")
+// }
+
+// var str = +("10")
+
+// console.log(typeof(str))
+
+
+var num = 10
+
+console.log(typeof(num.toString()))
