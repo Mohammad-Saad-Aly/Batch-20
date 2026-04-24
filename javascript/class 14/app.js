a0b9a12cb8ac813c6e32fada62acf20a29d8d297
hasanCoder66 <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2026-04-24 10:56:36 +0500

    24 0 2026

---
+++ javascript/class 14/app.js
@@ -0,0 +1,13 @@
+// var topiHua = new Date('14 aug, 2027')
+// var topiHai = new Date()
+// var minus = topiHai - topiHua
+// var age = minus / (1000 * 60 * 60 * 24 * 365)
+// console.log(topiHua)
+
+
+var days = ['sun', 'mon', 'tues', 'wed', 'thurs', 'fri', 'sat']
+
+var today = new Date()
+
+console.log()
+
