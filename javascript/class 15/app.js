0e1cafe7da80dcb18a5d17ad130a3b132535c291
hasanCoder66 <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2026-05-04 10:54:27 +0500

    4 may 26

---
+++ javascript/class 15/app.js
@@ -0,0 +1,15 @@
+// function table(){
+//     for(var i=1; i <= 10; i++){
+//         console.log(2 + " x " + i + " = " + 2 * i)
+//     }
+// }
+
+// for(var i=0; i < 3; i++){
+//     table()
+// }
+
+function calculator(a, b){
+    return a + b
+}
+
+console.log(calculator(10, 20, 30))
---
+++ javascript/class 16/app.js
@@ -0,0 +1,103 @@
+// function abc(){
+//     var someValue = "hello world"
+//     return someValue
+// }
+// function def(){
+//     return abc()
+// }
+// var getValue = def()
+// console.log(getValue)
+
+
+// local vs global variables 
+
+// var str = "hello world"
+
+// function foo() {
+//     str = "hello smit"
+//     console.log(str)
+// }
+
+// foo()
+
+// console.log(str)
+
+
+
+// var num = 10
+// function sum() {
+//     num += 20
+//     console.log(num)
+// }
+// sum()
+// console.log(num)
+
+// (function abc() {
+//     console.log("hello")
+// })()
+
+// IIFE
+
+
+// function calc(a, b, c){
+//     if(c == "+"){
+//         return a + b
+//     }
+//     else if(c == "-"){
+//         return a - b
+//     }
+// }
+
+// var plus = calc(10, 20, '+')
+// var minus = calc(10, 20, '-')
+// console.log(minus)
+
+
+// 4!
+// 4 x 3 x 2 x 1 = 
+// 6!
+// 6 x 5 x 4 x 3 x 2 x 1
+
+// var user = prompt("Enter any word")
+
+// task : check word palindrome or not
+
+// madam
+// 121
+// Mom
+// dad
+// civic
+
+
+
+var word = prompt("Enter any word")
+console.log(word.split('').reverse().join(''))
+
+// var check = ''
+
+// for (var i = word.length - 1; i >= 0; i--) {
+//     check += word[i]
+// }
+
+// if (check == word) {
+//     console.log(word + " word is palindrome")
+// }
+// else {
+//     console.log(word + " word is not palindrome")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
