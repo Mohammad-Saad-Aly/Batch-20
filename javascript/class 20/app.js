e6a6771f1c3833465f11f3d216aa1a74a15aca63
bilalteaching-byte <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2026-05-13 10:38:52 +0500

    13 may 2026

---
+++ javascript/class 20/app.js
@@ -0,0 +1,25 @@
+// hello world
+
+/*
+
+hello
+hello
+hello
+hello
+hello
+
+*/
+
+// var getImage = document.getElementById('img')
+
+// function tomBhaga(){
+//     getImage.className = "abc"
+// }
+
+// function kuch(){
+//     getImage.className = "re"
+// }
+
+// function hataya(){
+//     getImage.className = 'def'
+// }
