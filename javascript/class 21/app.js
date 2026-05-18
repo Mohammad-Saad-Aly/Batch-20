f6a9eab9e28a075a0b8fdb08199ff9b78ba5525d
Mohammad Saad Ali <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2026-05-18 10:49:56 +0500

    18 may 2026

--- javascript/class 21/app.js
+++ javascript/class 21/app.js
@@ -3,21 +3,40 @@
 // }
 
 
+var getInp = document.querySelector("#user")
 var getList = document.querySelector("#list")
 var getBtn = document.querySelector("#btn")
-getBtn.addEventListener('click', function(){
-    var getInp = document.querySelector("#user")
+getBtn.addEventListener('click', function () {
 
-    getList.innerHTML += `<li> ${getInp.value} <button onclick='delBtn(this)'> Delete </button> </li>`
+    getList.innerHTML += `<li> ${getInp.value} <button onclick='delBtn(this)'> Delete </button> <button onclick='editItem(this)'> Edit </button> </li>`
 
     getInp.value = ""
 
 })
 
-function delBtn(e){
+function delBtn(e) {
     e.parentNode.remove()
 }
 
-function delAll(){
+function delAll() {
     getList.innerHTML = ''
+}
+
+var currentItem;
+
+function editItem(e){
+    currentItem = e.parentNode.firstChild
+    getInp.value = e.parentNode.firstChild.textContent
+    document.getElementById("update").style.display = 'inline-block'
+}
+
+
+function updateValue(){
+    // value chahye field ki
+    // currentItem
+
+    currentItem.textContent = getInp.value
+    getInp.value = ''
+    document.getElementById("update").style.display = "none"
+
 }
