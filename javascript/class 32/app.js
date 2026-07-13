c28a26d70fcb77e949175f79ebe243ed5b7c3153
Mohammad Saad Ali <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2026-07-13 09:51:05 +0500

    class 32

---
+++ javascript/class 32/app.js
@@ -0,0 +1,38 @@
+
+// let myNum = nums.findIndex((data)=>{
+//     return data > 10
+// })
+
+// console.log(myNum)
+
+// let users = [
+//     { id: 1, name: "Kamran", age: 17 },
+//     { id: 2, name: "Topi", age: 18 },
+//     { id: 3, name: "Hasnain", age: 17 }
+// ]
+
+// let findUser = users.findIndex(user => user.name == "Hasnain")
+
+// if (findUser != -1) {
+//     users.splice(findUser, 1, { id: 3, name: "Saad", age: 26 })
+// }
+
+// console.log(users)
+
+// let nums = [10, [[40, 20], 50, 30], 80]
+
+// let myNums = nums.flat().flat().filter((data)=> data > 40).toString().split(',')
+
+// console.log(myNums)
+
+// let myNum = nums.every((data)=>{
+//     return data > 5
+// })
+
+
+// let myNum = nums.reduce((acc, currVal) => {
+//     return acc + currVal
+// })
+
+// console.log(myNum)
+
