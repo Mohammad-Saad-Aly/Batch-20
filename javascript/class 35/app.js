f0a8a66ded79d9fd84fea53ee3a5a9dafc816f41
Mohammad Saad Ali <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2026-07-17 10:58:49 +0500

    17 july 2026

---
+++ javascript/class 35/app.js
@@ -0,0 +1,123 @@
+// let obj = {
+//     id: 1,
+//     name: "ali",
+//     age: 26,
+//     sec: "B",
+//     obj2 : {
+//         abc: "xyz",
+//         def: "ghi"
+//     }
+// }
+
+// let {id, name, age, section} = obj
+
+// let {abc, def} = obj.obj2
+
+// console.log(def)
+
+
+
+// let [a, b, c, d] = fruits
+
+// console.log(c)
+
+// let products = [
+//     {
+//         id: 1,
+//         age: 70
+//     },
+//     {
+//         id: 2,
+//         age: 65
+//     }
+// ]
+
+// let [a, {id, age}] = products
+
+// let {id, age} = a
+
+// console.log(age)
+
+
+// function foo(a, b, ...c){
+//     let num = 0
+//     c.map((value)=>{
+//         num += value
+//     })
+//     return a + b + num
+// }
+
+// let getValue = foo(10, 20, 30, 40, 50)
+
+// console.log(getValue)
+
+
+// let fruits = ['mango', 'apple', 'orange', 'banana']
+
+// let copy = [...fruits]
+
+// copy.push("cherry")
+
+// console.log(fruits)
+
+
+// let copy = fruits
+
+// copy.push("cherry")
+
+// console.log(fruits)
+
+
+// let student1 = {
+//     id: 1,
+//     name: "rehman",
+//     age: 20
+// }
+
+// let student2 = {
+//     ...student1,
+//     roll: 123
+// }
+
+// console.log(student1)
+
+// let nums = [10, 20, 10, 30, 40, 10, 20, 60]
+
+// let task = new Set(nums)
+
+// task.add(50)
+// task.add(50)
+
+// console.log(task)
+// remove duplicate values
+
+// let test = new Map()
+// test.set('id', 1)
+// test.set(10, 20)
+
+// console.log(test.size)
+
+
+
+
+// first class function and default parameters
+// let value = function sum(a = 5, b = 10) {
+//     return a + b
+// }
+// let getValue = sum(30, 30)
+// console.log(getValue)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
